5c927819979ee87b4a624d47cb6568dc95539879
intannuraeni83 <intannuraeni83>
2024-02-20 07:35:38 +0700

    menyelesaikan sikom_1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 				<div class="wd-md-50p login d-none d-md-block page-signin-style p-5 text-white" >
 					<div class="my-auto authentication-pages">
 						<div>
-							<img src="../{{ asset('') }}assets/img/brand/logo-white.png" class=" m-0 mb-4" alt="">
+							<img src="{{ asset('') }}assets/img/brand/logo-white.png" class=" m-0 mb-4" alt="logo">
 							<h5 class="mb-4">Responsive Modern Dashboard &amp; Admin Template</h5>
 							<p class="mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 							<a href="index.html" class="btn btn-success">Learn More</a>
@@ -20,10 +20,10 @@
 						<h4>Please sign in to continue</h4>
 						<form action="index.html">
 							<div class="form-group">
-								<label>Email</label><input class="form-control" placeholder="Enter your email" type="text" value="info@spruko.com">
+								<label>Email</label><input class="form-control" placeholder="Enter your email" type="text" value="intannuraeni11220@gmail.com">
 							</div>
 							<div class="form-group">
-								<label>Password</label> <input class="form-control" placeholder="Enter your password" type="password" value="sprukodemo">
+								<label>Password</label> <input class="form-control" placeholder="Enter your password" type="password" value="rahasia">
 							</div><button class="btn btn-primary btn-block">Sign In</button>
 						</form>
 					</div>
